b016861d46e9336c8f5ef4c05dcae79d85fd76d1
Aakash <aku2162@gmail.com>
2016-07-15 16:20:27 +0530

    #362, problem 2

--- CodeForces/#362/Main2.java
+++ CodeForces/#362/Main2.java
@@ -1,20 +1,30 @@
 import java.util.*;
+import java.util.regex.*;
 import java.math.*;
 import java.io.*;
 
 public class Main2 {
 	public static void main(String args[]) {
-		Scanner scf = new Scanner(System.in);
-		Scanner scc = new Scanner(System.in);
-		Scanner scb = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-		scf.useDelimiter("\\.");
-		int a = scf.nextInt();
-		BigInteger d = scf.nextBigInteger();
+		Pattern p = Pattern.compile("((\\d).(\\d+))e(\\d+)");
+		String inp = sc.nextLine();
 
-		String e = scc.nextLine();
-		//int b = scb.nextInt();
+		Matcher m = p.matcher(inp);
+		boolean matches = m.matches();
 
-		System.out.println(""+a+" "+d.intValue()+" ");
+		if(m.group(3).equals("0") && m.group(4).equals("0"))
+		{
+			System.out.println(m.group(2));
+		}
+		else if(matches)
+		{
+			BigDecimal num = new BigDecimal(m.group(1));
+			BigDecimal exp = new BigDecimal(m.group(4));
+
+			BigDecimal ans = num.scaleByPowerOfTen(exp.intValue());
+
+			System.out.println(ans.toPlainString());
+		}
 	}
 }
